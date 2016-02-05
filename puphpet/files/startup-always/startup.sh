5fd987d3e95be54afb822ebd364a9da808260b9e
Scott Molinari <scott.molinari@adduco.de>
2016-02-05 08:29:46 +0100

    Correction to startup.sh too!

--- puphpet/files/startup-always/startup.sh
+++ puphpet/files/startup-always/startup.sh
@@ -2,7 +2,7 @@
 echo "------------ RUNNING STARTUP COMMANDS -------------"
 
 ### Set variables
-export ORIENT_VERSION="2.1.7"
+export ORIENT_VERSION="2.1.10"
 
 export INSTALL_DIR="/home/vagrant"
 export VAGRANT_DIR="/vagrant"
